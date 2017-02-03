34d55a0c612f1afe45c3abe4e7fc218c135c08d5
Asdrubal Lugo <asdrubal.a.l@gmail.com>
2017-02-03 11:34:17 -0400

    initial commit

---
+++ database/migrations/2017_02_03_152447_create_data_users_table.php
@@ -0,0 +1,44 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateDataUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('data_users', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('user_id')->unsigned();
+            $table->increments('id');
+            $table->char('document_type', 1);
+            $table->string('id_number', 8)->unique();
+            $table->string('name');
+            $table->date('birthdate');
+            $table->string('phone_number', 20);
+            $table->text('direction');
+            $table->timestamps();
+          
+            $table->foreign('user_id')->references('id')->on('users');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('data_users');
+    }
+}
+
+
+<?php
